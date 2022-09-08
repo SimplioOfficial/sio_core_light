a49e4483dad61f51145cf39cd624b2a287986fd5
ciripel <nedelcu.alexandru@yahoo.com>
2022-09-08 18:45:26 +0300

    fix: improve utxo Exceptions

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,16 +1,24 @@
-/// All the exceptions are defined so application can
-/// respond differently to each case.
-///
+// Specific exceptions are defined so application can
+// respond differently to each case.
+
 /// Current address has no utxo available.
 class NoUtxoAvailableException implements Exception {
-  NoUtxoAvailableException();
+  const NoUtxoAvailableException();
+  @override
+  String toString() =>
+      'NoUtxoAvailableException: Current address has no utxo available.';
 }
 
 /// Total amount in the address is lower than the amount
-/// that is wanted to be send plus the transaction fee
+/// that is wanted to be sent plus the transaction fee
 /// needed to execute this transaction.
 ///
 /// Try to lower the amount to be sent.
 class LowTotalAmountPlusFeeException implements Exception {
-  LowTotalAmountPlusFeeException();
+  const LowTotalAmountPlusFeeException();
+  @override
+  String toString() => 'LowTotalAmountPlusFeeException: '
+      'Total amount in the address is lower than the amount that is wanted to '
+      'be send plus the transaction fee needed to execute this transaction. '
+      'Try to lower the amount to be sent.';
 }
