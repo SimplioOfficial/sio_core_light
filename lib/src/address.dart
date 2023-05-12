8034f0d431fda12eef903e064975944d89b3cec8
ciripel <nedelcu.alexandru@yahoo.com>
2023-05-12 12:15:06 +0300

    implement address parser

--- lib/src/address.dart
+++ lib/src/address.dart
@@ -6,22 +6,31 @@ class Address {
     required String address,
     required int networkId,
   }) {
+    return AnyAddress.isValid(address, networkId);
+  }
+
+  static String parse({
+    required String address,
+    required int networkId,
+  }) {
+    if (address.length < 10) return address;
+
     if (Cluster.ethLegacyAndEIP1559.contains(networkId)) {
-      if (address.substring(1, 9).toLowerCase() == 'ethereum:') {
-        address = address.substring(10);
+      if (address.substring(0, 9).toLowerCase() == 'ethereum:') {
+        address = address.substring(9);
       }
 
-      if (address.substring(1, 4).toLowerCase() == 'bnb:') {
-        address = address.substring(5);
+      if (address.substring(0, 4).toLowerCase() == 'bnb:') {
+        address = address.substring(4);
       }
     }
 
     if (Cluster.utxo.contains(networkId)) {
-      if (address.substring(1, 8) == 'bitcoin:') {
-        address = address.substring(9);
+      if (address.substring(0, 8) == 'bitcoin:') {
+        address = address.substring(8);
       }
     }
 
-    return AnyAddress.isValid(address, networkId);
+    return address;
   }
 }
