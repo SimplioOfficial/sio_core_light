5936f4c539298d43c92547a6ac9f3660fe6f6403
ciripel <37701673+ciripel@users.noreply.github.com>
2023-05-12 12:33:00 +0300

    feat/add-address-url-parser-support (#27)

--- lib/src/address.dart
+++ lib/src/address.dart
@@ -1,3 +1,4 @@
+import 'package:sio_core_light/sio_core_light.dart';
 import 'package:trust_wallet_core_lib/trust_wallet_core_lib.dart';
 
 class Address {
@@ -6,5 +7,30 @@ class Address {
     required int networkId,
   }) {
     return AnyAddress.isValid(address, networkId);
+  }
+
+  static String parse({
+    required String address,
+    required int networkId,
+  }) {
+    if (address.length < 10) return address;
+
+    if (Cluster.ethLegacyAndEIP1559.contains(networkId)) {
+      if (address.substring(0, 9).toLowerCase() == 'ethereum:') {
+        address = address.substring(9);
+      }
+
+      if (address.substring(0, 4).toLowerCase() == 'bnb:') {
+        address = address.substring(4);
+      }
+    }
+
+    if (Cluster.utxo.contains(networkId)) {
+      if (address.substring(0, 8) == 'bitcoin:') {
+        address = address.substring(8);
+      }
+    }
+
+    return address;
   }
 }
